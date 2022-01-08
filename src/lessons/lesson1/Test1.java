b7e14b28c21aef6dc505b3d9453fe2149e4dd76d
Mukhammad Jalolov <muhammadzalolov@gmail.com>
2022-01-08 14:15:11 +0300

    1

--- src/lessons/lesson1/Test1.java
+++ src/lessons/lesson1/Test1.java
@@ -3,8 +3,8 @@ package lessons.lesson1;
 public class Test1 {
     public static void main(String[] args) {
 
-        System.out.println("abcdef");
-        System.out.println();
+//        System.out.println("abcdef");
+//        System.out.println();
         System.out.println("ghjikl");
 
         System.out.println();
