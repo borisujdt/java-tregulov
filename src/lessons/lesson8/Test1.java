c50f100d0fe5d24a48db44d97950b416d92984de
Mukhammad Jalolov <muhammadzalolov@gmail.com>
2022-02-10 17:51:04 +0300

    1

---
+++ src/lessons/lesson8/Test1.java
@@ -0,0 +1,24 @@
+package lessons.lesson8;
+
+public class Test1 {
+    public final int a;
+
+    Test1(){
+        a = 10;
+    }
+
+    Test1(boolean b){
+        a = 15;
+    }
+    public void abc(final short s){
+        final byte b;
+        b = 10;
+        System.out.println(s+b);
+    }
+
+    public static void main(String[] args) {
+        Test1 t = new Test1();
+
+        System.out.println(t.a);
+    }
+}
