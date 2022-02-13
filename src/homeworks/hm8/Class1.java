462f66061efadc5ff2684b56f813757f564953c8
Mukhammad Jalolov <muhammadzalolov@gmail.com>
2022-02-13 14:06:08 +0300

    1

---
+++ src/homeworks/hm8/Class1.java
@@ -0,0 +1,27 @@
+package homeworks.hm8;
+
+public class Class1 {
+    public static int umnojenie(int a, int b, int c){
+        int x = a*b*c;
+        return x;
+    }
+    public static void delenie(int a, int b){
+
+        int y = a/b;
+        int z = a%b;
+        System.out.println("Целое число="+y);
+        System.out.println("Остаток="+z);
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(umnojenie(10,5,20));
+        delenie(10,3);
+
+    }
+
+    
+}
+
+
