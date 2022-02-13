f28362edf8c8282efab3dac3a9ccf8d9f4593ec4
m-jalolov <zhalolovmira@gmail.com>
2022-02-13 20:10:13 +0700

    7

--- src/homeworks/hm8/Class2.java
+++ src/homeworks/hm8/Class2.java
@@ -12,13 +12,31 @@ public class Class2 {
         return l;
     }
 
+    public static int umnojenie(int a, int b, int c){
+        int x = a*b*c;
+        return x;
+    }
+    public static void delenie(int a, int b){
+
+        int y = a/b;
+        int z = a%b;
+        System.out.println("Целое число="+y);
+        System.out.println("Остаток="+z);
+
+    }
 }
 
 class Test{
     public static void main(String[] args) {
         Class2 sl2 = new Class2();
         System.out.println(sl2.ploshadCruga(190.5));
+
         System.out.println(Class2.dlinaCruga(140.7));
+
+        System.out.println(Class2.umnojenie(10,5,20));
+
+        Class2.delenie(10,3);
+        Class2.delenie(50,17);
     }
 }
 
