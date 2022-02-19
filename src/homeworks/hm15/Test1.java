872a7fb929d8065b74dceeff42b2759e887e36a9
m-jalolov <zhalolovmira@gmail.com>
2022-02-19 22:54:23 +0700

    7

---
+++ src/homeworks/hm15/Test1.java
@@ -0,0 +1,43 @@
+package homeworks.hm15;
+
+public class Test1 {
+
+    public static void time() {
+        int hour = 0;
+
+        OUTER:
+        while (hour < 6) {
+
+            int minute = -1;
+            MIDDLE:
+            do {
+                minute++;
+
+                if (hour > 1 && minute % 10 == 0) {
+                    break OUTER;
+                }
+
+            int seconds = 0;
+            INNER:
+            while (seconds < 60) {
+                if (seconds * hour > minute) {
+                    continue MIDDLE;
+                }
+                System.out.println(hour + ":" + minute + ":" + seconds);
+                seconds++;
+            }
+
+        }while (minute<59);
+            hour++;
+
+    }
+
+
+    }
+
+    public static void main(String[] args) {
+        time();
+    }
+}
+
+
