59f37a01a14170856fc20eb6d0653f7dc121e4e1
m-jalolov <zhalolovmira@gmail.com>
2022-03-01 20:48:02 +0700

    7

---
+++ src/lessons/lesson17/Test3.java
@@ -0,0 +1,14 @@
+package lessons.lesson17;
+
+public class Test3 {
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("Hello");
+        StringBuffer sb2 = new StringBuffer("Good bye!");
+
+        String s1 = new String(sb1);
+        String s2 = new String(sb2);
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
