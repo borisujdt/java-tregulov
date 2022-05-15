3095cfb7e68eb06e7f1469d665e37b3fdb7c8669
m-jalolov <zhalolovmira@gmail.com>
2022-05-15 21:16:01 +0700

    7

--- src/interfaces/Animal.java
+++ src/interfaces/Animal.java
@@ -1,4 +1,16 @@
 package interfaces;
 
-public class Animal {
+public class Animal implements Info {
+    public int id;
+
+    public Animal(int id){
+        this.id = id;
+    }
+
+    public void sleep() {
+        System.out.println("I am sleeping");
+    }
+    public void showInfo(){
+        System.out.println("Id is "+this.id);
+    }
 }
